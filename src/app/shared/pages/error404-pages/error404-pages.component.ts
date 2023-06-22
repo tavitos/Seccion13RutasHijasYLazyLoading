e7d9ca01df24e45e20f817f98fa897ef681ef734
tavitos <lesamaoyabi@gmail.com>
2023-06-22 09:02:25 -0600

    Proyecto HeroesApp

---
+++ src/app/shared/pages/error404-pages/error404-pages.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error404-pages',
+  templateUrl: './error404-pages.component.html',
+  styles: [
+  ]
+})
+export class Error404PagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Error404PagesComponent } from './pages/error404-pages/error404-pages.component';
+
+
+
+@NgModule({
+  declarations: [
+    Error404PagesComponent
+  ],
+  exports: [
+    Error404PagesComponent
+  ]
+})
+export class SharedModule { }
